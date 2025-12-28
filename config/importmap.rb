2d39e8a55c88d3fdde20b25a0418f41f864a3711
Artyom Pechyorin <pecha7x@gmail.com>
2025-12-28 21:57:41 +0300

    Implement Navbar

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,4 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@rails/actiontext", to: "actiontext.esm.js"
 pin "lexxy", to: "lexxy.js"
-pin "@rails/activestorage", to: "activestorage.esm.js" # to support attachments
+pin "@rails/activestorage", to: "activestorage.esm.js"
