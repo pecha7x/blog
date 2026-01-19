b8300783112b45d024fcfb01cd431e19ca3ae1fe
Artyom Pechyorin <pecha7x@gmail.com>
2026-01-19 19:58:40 +0300

    about me page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,8 @@
 class StaticPagesController < ApplicationController
   def about
-  end
-
-  def contact
+    now = Time.now.utc.to_date
+    dob = Date.new(1988, 4, 19)
+    @age = now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
   end
 
   def home
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   get '/about', to: 'static_pages#about'
-  get '/contact', to: 'static_pages#contact'
   get '/home', to: 'static_pages#home'
 end
