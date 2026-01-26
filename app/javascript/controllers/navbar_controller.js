45417f22154f791d902d17eafe5ebc0f07debf0b
Artyom Pechyorin <pecha7x@gmail.com>
2026-01-26 12:25:51 +0300

    Mobile menu close sections

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -1,47 +1,28 @@
+// controllers/gallery_controller.js
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="navbar"
 export default class extends Controller {
-  static targets = ["content"]
-  connect() {
-    // this.close()
-  }
-
-  closeWithKeyboard(event) {
-    if (event.key === "Escape") {
-      this.close()
+  next(event) {
+    var target = event.target;
+    if (target.closest("a")) {
+      return;
     }
-  }
 
-  closeOnBigScreen(_event) {
-    if (window.innerWidth > 768) {
-      this.close()
-      console.log('closeOnBigScreen')
+    event.preventDefault();
+
+    var currentTarget = event.currentTarget;
+    var items = this.element.getElementsByTagName("details");
+
+    for (var i = 0; i < items.length; i++) {
+      if (currentTarget != items[i] && items[i].hasAttribute("open")) {
+        items[i].removeAttribute("open");
+      }
     }
-  }
 
-  toggle() {
-    if (this.contentTarget.classList.contains('hidden')) {
-      this.open()
+    if (currentTarget.hasAttribute("open")) {
+      currentTarget.removeAttribute("open");
     } else {
-      this.close()
+      currentTarget.setAttribute("open", '1');
     }
   }
-
-  open() {
-    this.contentTarget.classList.remove('hidden')
-    let main = document.querySelector('main')
-    main.classList.add('blur')
-    document.body.classList.add('overflow-hidden')
-    // main.classList.add('hidden')
-  }
-
-  close() {
-    this.contentTarget.classList.add('hidden')
-    let main = document.querySelector('main')
-    main.classList.remove('blur')
-    document.body.classList.remove('overflow-hidden')
-    // main.classList.remove('hidden')
-  }
-
 }
