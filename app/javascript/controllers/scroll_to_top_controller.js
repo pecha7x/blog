70c1c3631b9df68852c8dda9d9f99fd13e51f889
Artyom Pechyorin <pecha7x@gmail.com>
2026-01-26 12:42:19 +0300

    scroll to top and fix init preline

--- app/javascript/controllers/scroll_to_top_controller.js
+++ app/javascript/controllers/scroll_to_top_controller.js
@@ -2,6 +2,8 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    this.element.textContent = "Hello World!"
+    window.onbeforeunload = function () {
+      window.scrollTo(0, 0);
+    }
   }
 }
