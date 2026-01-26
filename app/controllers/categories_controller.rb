55788c8d25522e05853a7328a9c160a5b5aa3932
Artyom Pechyorin <pecha7x@gmail.com>
2026-01-26 11:13:45 +0300

    Change tree collapse icon

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,4 @@
 class CategoriesController < ApplicationController
-  # add_breadcrumb "about", :about_path
-
   def show
     @category = Category.friendly.find(params.expect(:id))
     add_breadcrumb "View More", category_articles_path(@category)
